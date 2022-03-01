6373a831fa3a6b36cd1ce968c0d069fe102e14db
fathiaer <100201242+fathiaer@users.noreply.github.com>
2022-03-01 16:10:22 +0700

    Add files via upload

---
+++ ProcessAName5026211067.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class ProcessAName5026211067{
+
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Type your name: ");
+		String name = input.nextLine();
+		int spaceIdx = name.indexOf(" ");
+		String lastName = name.substring(spaceIdx +1, name.length());
+		String initial = name.substring(0, 1);
+		String finalName = lastName + "," + initial + ".";
+		System.out.println("Your name is: " +finalName);
+	}
+
+}
